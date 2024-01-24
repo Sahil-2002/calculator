34c884a7c03e89cee6e834c8747510161ee3682e
Sahil-2002 <sahila.khandekar@gmail.com>
2024-01-24 17:15:14 +0530

    hhjh

---
+++ src/project/calculator.java
@@ -0,0 +1,10 @@
+package project;
+
+public class calculator {
+    public static void main(String[] args) {
+        System.out.println("welcome to my calculator ");
+        int a= 10, b=20;
+       int  add= a+b;
+        System.out.println("addition is "+(add));
+    }
+}
